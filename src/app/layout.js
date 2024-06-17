e7eb7d9874cc5567cf530229053953f9701b19a0
JJozef <Josemillaquen12@gmail.com>
2024-06-16 23:47:11 -0400

    chore: update metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,45 @@ const fontSans = FontSans({
 
 export const metadata = {
   title: 'Telsur | Pago de Cuentas',
-  description: 'Telsur Pagos'
+  metadataBase: new URL('https://telsur-pagos.vercel.app'),
+  description: 'Paga tus cuentas de Telsur de forma rápida y segura.',
+  keywords: ['Telsur', 'Pago de Cuentas', 'Pago de Servicios'],
+  alternates: {
+    canonical: new URL('https://telsur-pagos.vercel.app')
+  },
+  authors: [
+    {
+      name: 'Jose Ignacio',
+      url: 'https://joseignacio.dev'
+    }
+  ],
+  creator: 'Jose Ignacio',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://telsur-pagos.vercel.app',
+    title: 'Telsur | Pago de Cuentas',
+    description: 'Paga tus cuentas de Telsur de forma rápida y segura.',
+    siteName: 'Telsur | Pago de Cuentas',
+    images: [
+      {
+        url: 'https://telsur-pagos.vercel.app/og.png',
+        width: 1200,
+        height: 630,
+        alt: 'Telsur | Pago de Cuentas'
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Telsur | Pago de Cuentas',
+    description: 'Paga tus cuentas de Telsur de forma rápida y segura.',
+    images: ['https://telsur-pagos.vercel.app/og.png'],
+    creator: '@jozefzin'
+  },
+  icons: {
+    icon: '/favicon.ico'
+  }
 }
 
 export default function RootLayout({ children }) {
